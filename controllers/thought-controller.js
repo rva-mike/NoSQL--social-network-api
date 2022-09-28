261762554ec1ab571ff905146e8698838c40939c
rva-mike <mike.levy.rva@gmail.com>
2022-09-27 20:11:05 -0400

    updated getThoughtById

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -19,20 +19,40 @@ const thoughtController = {
     },
 
     //get one thought by ID
-    getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
-            .populate({
-                path: 'user',
-                select: '-__v'
-            })
-           .select('-__v')
-           .sort({ _id: -1 })
-           .then(dbThoughtData => res.json(dbThoughtData))
-           .catch(err => {
-               console.log(err);
-               res.status(500).json(err)
-           })
-    },
+    // getThoughtById({ params }, res) {
+    //     Thought.findOne({ _id: params.id })
+    //         .populate({
+    //             path: 'user',
+    //             select: '-__v'
+    //         })
+    //        .select('-__v')
+    //        .sort({ _id: -1 })
+    //        .then(dbThoughtData => res.json(dbThoughtData))
+    //        .catch(err => {
+    //            console.log(err);
+    //            res.status(500).json(err)
+    //        })
+    // },
+
+
+//   getThoughtById
+getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => {
+        // if no thought is found
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this ID" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+
 
     //create thought
     createThought({ params, body }, res) {
